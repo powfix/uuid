c913df5f70f2b00eaeec66a384d0670327127a0f
POWFIX <powfix@gmail.com>
2025-09-17 17:29:12 +0900

    Cleanup

--- tsup.esm.config.ts
+++ tsup.esm.config.ts
@@ -1,9 +1,4 @@
 import { defineConfig } from 'tsup';
-import packageJson from './package.json';
-
-const dependencies = Object.keys(packageJson.dependencies || {});
-const devDependencies = Object.keys(packageJson.devDependencies || {});
-const peerDependencies = Object.keys(packageJson.peerDependencies || {});
 
 export default defineConfig({
   entry: ['src/**/*.ts'],
@@ -13,17 +8,4 @@ export default defineConfig({
   dts: true,
   treeshake: true,
   bundle: false,
-
-  // entry: ['src/index.ts', 'src/index.node.ts'],
-  // format: ['esm', 'cjs'],
-  // dts: true,
-  // clean: true,
-  // splitting: false,
-  // sourcemap: false,
-  // minify: false,
-  // external: [
-  //   ...dependencies,
-  //   ...devDependencies,
-  //   ...peerDependencies,
-  // ],
 });
