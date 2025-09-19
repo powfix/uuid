ffd654c095470b1bfa2ecc89f5f0a20aba29c414
POWFIX <powfix@gmail.com>
2025-09-19 15:21:00 +0900

    Add type NullableUuidInput NonNullableUuidInput

--- src/shared/types/UuidInput.ts
+++ src/shared/types/UuidInput.ts
@@ -2,3 +2,5 @@ import {UUID} from "../UUID";
 import {UuidString} from "./UuidString";
 
 export type UuidInput = string | UuidString | ArrayBufferView | UUID;
+export type NullableUuidInput = UuidInput | null | undefined;
+export type NonNullableUuidInput = NonNullable<NullableUuidInput>;
